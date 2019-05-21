57e99a2a12c9123d51f70f5e10efd57c944d2f96
Paweł <pawel.koelmer@gmail.com>
2019-05-21 22:57:20 +0200

    Początek tworzenia Layout, dodana obsługa SCSS.

--- Projekt/Projekt/Models/DAL/ShopInitializer.cs
+++ Projekt/Projekt/Models/DAL/ShopInitializer.cs
@@ -12,6 +12,7 @@ namespace Projekt.Models.DAL
         protected override void Seed(ShopContext context)
         {
             SeedRolesData(context);
+            SeedItemsData(context);
             base.Seed(context);
         }
         private void SeedRolesData(ShopContext context)
@@ -24,6 +25,16 @@ namespace Projekt.Models.DAL
                 new Role() { roleName="premium"}
             };
             Roles.ForEach(r => context.Roles.Add(r));
+            context.SaveChanges();
+        }
+        private void SeedItemsData(ShopContext context)
+        {
+            var items = new List<Item>
+            {
+                new Item() { productName="Czekolada",inStock=50, avaiablity=true,productDescription="Najlepsza czekolada na rynku",productPrice=10,productScore=2,},
+                
+            };
+            items.ForEach(i => context.Items.Add(i));
             context.SaveChanges();
         }
     }
